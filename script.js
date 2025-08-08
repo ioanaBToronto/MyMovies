1056853630bb5fc527ddf89b5dcc7046d801b86e
ioanaBToronto <ioana.belchita@gmail.com>
2025-08-08 15:33:15 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const form = document.getElementById('movie-form');
+const movieList = document.getElementById('movie-list');
+let movies = [];
+
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+  const title = document.getElementById('title').value;
+  const rating = document.getElementById('rating').value;
+  movies.push({ title, rating });
+  displayMovies();
+  form.reset();
+});
+
+function displayMovies() {
+  movieList.innerHTML = '';
+  movies.forEach((movie) => {
+    movieList.innerHTML += `<div>${movie.title} - Rating: ${movie.rating}/10</div>`;
+  });
+}
